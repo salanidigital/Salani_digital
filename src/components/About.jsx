c031e83497e121102fffa82171d1b5481453ba66
Salani Digital <salanidigitalchannel@gmail.com>
2026-09-15 13:28:43 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,7 +17,7 @@ export default function About() {
           {/* S.Digital Logo */}
           <div className="about-logo">
             <img
-              src="src/public/rest.png"
+              src="/rest.png"
               alt="S.Digital"
             />
           </div>
