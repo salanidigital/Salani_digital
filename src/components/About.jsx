0b9d7c98d14cbe7374705dae7bc30698c082f859
Salani Digital <salanidigitalchannel@gmail.com>
2026-09-16 13:19:43 +0530

    Fix image source and improve About section text
    Updated image source and refined text content.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,16 +11,15 @@ export default function About() {
         <div className="about-statement reveal">
 
           <span className="big-number">
-          
           </span>
 
           {/* S.Digital Logo */}
           <div className="about-logo">
             <img
-src="src/public/Footer900.png"              alt="S.Digital"
+              src="https://raw.githubusercontent.com/salanidigital/Salani_digital/main/src/public/Footer900.png"
+              alt="S.Digital"
             />
           </div>
-
 
         </div>
 
@@ -31,16 +30,12 @@ src="src/public/Footer900.png"              alt="S.Digital"
           </p>
 
           <p>
-            Salani Digital helps Indian and international            
+            Salani Digital helps Indian and international
+            businesses grow through online marketing
+            strategies.
 
-            businesses grow through online marketing           
-
-            strategies.            
- Salani Digital is a professional            
-
-            digital marketing            
-agency helping businesses           
-
+            Salani Digital is a professional
+            digital marketing agency helping businesses
             grow online with result-driven marketing
             strategies.
           </p>
